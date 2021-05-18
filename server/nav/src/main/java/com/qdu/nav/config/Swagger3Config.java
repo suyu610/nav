2d869850a9f71f124bea24102f501af353d018a3
suyu_admin <372799030@qq.com>
2021-05-19 00:56:09 +0800

    +swgger3

---
+++ server/nav/src/main/java/com/qdu/nav/config/Swagger3Config.java
@@ -0,0 +1,10 @@
+package com.qdu.nav.config;/**
+  * @ClassName Swagger3Config
+  * @Description TODO
+  * @Author uuorb  
+  * @Date 2021/5/19 12:47 上午
+  * @Version 0.1
+ **/
+
+public class Swagger3Config {
+}
